0a54a0efe08116f485e12b1a77aa657be98cbf0b
jasonbriggs <jasonrastrickbriggs@gmail.com>
2015-10-13 13:18:10 -0400

    read meta client location and loading image
    the loading image will be replaced with an animation as soon as possible

--- meta-wrapper/main.js
+++ meta-wrapper/main.js
@@ -1,6 +1,9 @@
 var app = require('app'); // Module to control application life.
 var BrowserWindow = require('browser-window'); // Module to create native browser window.
 var Menu = require("menu");
+var shell = require('shell');
+var fs = require('fs');
+var os = require('os');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
@@ -22,10 +25,22 @@ app.on('before-quit', function() {
     mainWindow = null;
 });
 
+app.on("open-file", function(event, path) {
+    event.preventDefault();
+    filepath = path;
+
+    if (ready) {
+        mainWindow.webContents.send('open-file', filepath);
+        filepath = null;
+
+        return;
+    }
+});
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
+
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 1200,
@@ -42,176 +57,151 @@ app.on('ready', function() {
             mainWindow.hide();
             event.preventDefault()
         }
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-
     });
 
-    var template = [
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'CmdOrCtrl+Z',
-        role: 'undo'
-      },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        role: 'redo'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Cut',
-        accelerator: 'CmdOrCtrl+X',
-        role: 'cut'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'CmdOrCtrl+C',
-        role: 'copy'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'CmdOrCtrl+V',
-        role: 'paste'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'CmdOrCtrl+A',
-        role: 'selectall'
-      },
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.reload();
+    //find and launch the meta client application
+    var clientLocationFile = os.homedir(); + "/meta_client_location.txt";
+    fs.readFile(clientLocationFile, function(err, data) {
+        if (data && !err) {
+            shell.openItem(data.toString());
+        } else {
+            //meta client not installed or file system not accessible
         }
-      },
-      {
-        label: 'Toggle Full Screen',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Ctrl+Command+F';
-          else
-            return 'F11';
-        })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-        }
-      },
-      {
-        label: 'Toggle Developer Tools',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Alt+Command+I';
-          else
-            return 'Ctrl+Shift+I';
-        })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.toggleDevTools();
-        }
-      },
-    ]
-  },
-  {
-    label: 'Window',
-    role: 'window',
-    submenu: [
-      {
-        label: 'Minimize',
-        accelerator: 'CmdOrCtrl+M',
-        role: 'minimize'
-      },
-      {
-        label: 'Close',
-        accelerator: 'CmdOrCtrl+W',
-        role: 'close'
-      },
-    ]
-  },
-  {
-    label: 'Help',
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click: function() { require('shell').openExternal('http://electron.atom.io') }
-      },
-    ]
-  },
-];
+    });
 
-if (process.platform == 'darwin') {
-  var name = require('app').getName();
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        label: 'About ' + name,
-        role: 'about'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Services',
-        role: 'services',
-        submenu: []
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Hide ' + name,
-        accelerator: 'Command+H',
-        role: 'hide'
-      },
-      {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        role: 'hideothers'
-      },
-      {
-        label: 'Show All',
-        role: 'unhide'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click: function() { app.quit(); }
-      },
-    ]
-  });
-  // Window menu.
-  template[3].submenu.push(
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Bring All to Front',
-      role: 'front'
+    var template = [{
+        label: 'Edit',
+        submenu: [{
+            label: 'Undo',
+            accelerator: 'CmdOrCtrl+Z',
+            role: 'undo'
+        }, {
+            label: 'Redo',
+            accelerator: 'Shift+CmdOrCtrl+Z',
+            role: 'redo'
+        }, {
+            type: 'separator'
+        }, {
+            label: 'Cut',
+            accelerator: 'CmdOrCtrl+X',
+            role: 'cut'
+        }, {
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy'
+        }, {
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste'
+        }, {
+            label: 'Select All',
+            accelerator: 'CmdOrCtrl+A',
+            role: 'selectall'
+        }, ]
+    }, {
+        label: 'View',
+        submenu: [{
+            label: 'Reload',
+            accelerator: 'CmdOrCtrl+R',
+            click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.reload();
+            }
+        }, {
+            label: 'Toggle Full Screen',
+            accelerator: (function() {
+                if (process.platform == 'darwin')
+                    return 'Ctrl+Command+F';
+                else
+                    return 'F11';
+            })(),
+            click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+        }, {
+            label: 'Toggle Developer Tools',
+            accelerator: (function() {
+                if (process.platform == 'darwin')
+                    return 'Alt+Command+I';
+                else
+                    return 'Ctrl+Shift+I';
+            })(),
+            click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.toggleDevTools();
+            }
+        }, ]
+    }, {
+        label: 'Window',
+        role: 'window',
+        submenu: [{
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        }, {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        }, ]
+    }, {
+        label: 'Help',
+        role: 'help',
+        submenu: [{
+            label: 'Learn More',
+            click: function() {
+                require('shell').openExternal('http://electron.atom.io')
+            }
+        }, ]
+    }, ];
+
+    if (process.platform == 'darwin') {
+        var name = require('app').getName();
+        template.unshift({
+            label: name,
+            submenu: [{
+                label: 'About ' + name,
+                role: 'about'
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Services',
+                role: 'services',
+                submenu: []
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Hide ' + name,
+                accelerator: 'Command+H',
+                role: 'hide'
+            }, {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                role: 'hideothers'
+            }, {
+                label: 'Show All',
+                role: 'unhide'
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function() {
+                    app.quit();
+                }
+            }, ]
+        });
+        // Window menu.
+        template[3].submenu.push({
+            type: 'separator'
+        }, {
+            label: 'Bring All to Front',
+            role: 'front'
+        });
     }
-  );
-}
 
-menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 
 });
 
@@ -220,7 +210,8 @@ Menu.setApplicationMenu(menu);
 // Basically when the app icon is clicked and no windows are open.
 
 app.on('activate-with-no-open-windows', function() {
-    // Create the browser window.
+    // if app icon is clicked and no windows are shown, show the
+    // main window where it left off and focus on search bar
     mainWindow.show();
     mainWindow.focusOnWebView()
     mainWindow.webContents.send('focus', 'searchbar');
